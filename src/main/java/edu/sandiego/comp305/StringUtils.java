4f2a677cad95d929b7dee40bb0af958d75f7efe3
Jacob Penticoff <jpenticoff@sandiego.edu>
2026-04-26 22:37:56 -0700

    refactor: Fixed the formatting and access modifers of StringUtils so that it passes the checkstyle and spotbug requirements.

--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -2,8 +2,11 @@ package edu.sandiego.comp305;
 
 public class StringUtils {
 
-    static String reverseString(String stringToReverse){
-        StringBuilder reversedString = new StringBuilder();
+    private StringUtils() {
+    }
+
+    public static String reverseString(final String stringToReverse){
+        final StringBuilder reversedString = new StringBuilder();
 
         for (int i = stringToReverse.length() - 1; i >= 0; i--) {
             reversedString.append(stringToReverse.charAt(i));
@@ -12,7 +15,7 @@ public class StringUtils {
         return reversedString.toString();
     }
 
-    public static void main(String[] args){
+    public static void main(final String[] args){
         System.out.print(reverseString(args[0]));
     }
 
